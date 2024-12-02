c93daeb5f5bd8e116bc3018d866d9f366c14fa78
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-12-03 02:56:24 +0700

    feat(ecc): :sparkles: add encryption and decryption

---
+++ frontend/src/api/modules/ecc.api.js
@@ -0,0 +1,61 @@
+import Client from "../client.api";
+
+const ECC = {
+  async pointAdd(data) {
+    try {
+      const response = await Client.post("ecc/point-add", data); // Gọi API add
+      return response;
+    } catch (error) {
+      if (error.response) {
+        return error.response;
+      }
+    }
+  },
+
+  async isPointOnCurve(data) {
+    try {
+      const response = await Client.post("ecc/point-on-curve", data); 
+      return response;
+    } catch (error) {
+      if (error.response) {
+        return error.response;
+      }
+    }
+  },
+
+  async pointMultiply(data) {
+    try {
+      const response = await Client.post("ecc/point-multiply", data); 
+      return response;
+    } catch (error) {
+      if (error.response) {
+        return error.response;
+      }
+    }
+  },
+
+  async encrypt(data) {
+    try {
+      const response = await Client.post("ecc/encrypt", data); 
+      return response;
+    } catch (error) {
+      if (error.response) {
+        return error.response;
+      }
+    }
+  },
+
+  async decrypt(data) {
+    try {
+      const response = await Client.post("ecc/decrypt", data); 
+      return response;
+    } catch (error) {
+      if (error.response) {
+        return error.response;
+      }
+    }
+  }
+  
+};
+
+export default ECC;
