1e0529d738ef838d625dff7e0f638284175611a9
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-12-03 03:00:39 +0700

    feat(ecdsa): :sparkles: add ECDSA page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx"; // Import Layout component
 import ECC from "./pages/ECC.jsx";
-import RSA from './pages/RSA.jsx';  
-import ElGamal from './pages/Elgamal.jsx';
-import Homepage from './pages/Homepage.jsx';
-import Factors from './pages/Factors.jsx';
+import RSA from "./pages/RSA.jsx";
+import ElGamal from "./pages/Elgamal.jsx";
+import ECDSA from "./pages/ECDSA.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import Factors from "./pages/Factors.jsx";
 
 import { createTheme, ThemeProvider } from "@mui/material";
 
@@ -28,6 +29,7 @@ const App = () => {
             <Route path="/rsa" element={<RSA />} />
             <Route path="/ecc" element={<ECC />} />
             <Route path="/factors" element={<Factors />} />
+            <Route path="/ecdsa" element={<ECDSA />} />
           </Route>
         </Routes>
       </BrowserRouter>
