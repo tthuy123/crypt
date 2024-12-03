5131c6250fea2015dc8eb4862e59c2ae5e15880a
thuthuy <125456003+tthuy123@users.noreply.github.com>
2024-12-04 02:02:12 +0700

    fix: elgamal formular and add route for sig

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import RSA from './pages/RSA.jsx';
 import ElGamal from './pages/Elgamal.jsx';
 import Homepage from './pages/Homepage.jsx';
 import Factors from './pages/Factors.jsx';
+import ElGamalSignature from './pages/Elgamal_Signature.jsx';
 
 import { createTheme, ThemeProvider } from "@mui/material";
 import RSA_Signature from "./pages/RSA_Signature.jsx";
@@ -30,6 +31,7 @@ const App = () => {
             <Route path="/rsa-signature" element={<RSA_Signature />} />
             <Route path="/ecc" element={<ECC />} />
             <Route path="/factors" element={<Factors />} />
+            <Route path="/elgamal-signature" element={<ElGamalSignature />} />
           </Route>
         </Routes>
       </BrowserRouter>
